5bc47cd31f9dbbf7f05d481658d7ce62d3695bcc
LAPTOP-6UF1OQKL\anton <antonio.barahona@grupoprogestion.com>
2021-04-21 18:43:05 -0400

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      Curso para Programador FullStack cambios
+      Curso para Programador FullStack
     </footer>
   );
 }
