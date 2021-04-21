fb0318d534f127e8b12d2faccab3274b9657eacf
Santiago Anrango <santy2516@gmail.com>
2021-04-21 18:22:35 -0500

    modificación de texto en footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      Pie de pagina clase 23 Santiago Anrango
+      Pie de pagina clase 23 Santiago Anrango<br/>
+      commit subido a rama develop
     </footer>
   );
 }
